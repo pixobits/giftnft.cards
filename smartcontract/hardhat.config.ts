c3352b416000ea399aa1050f61ae76bcf935034f
Sharad Chand <pepsighan@sharadchand.com>
2021-12-22 18:42:10 +0545

    Update the config to upgrade the contract

--- smartcontract/hardhat.config.ts
+++ smartcontract/hardhat.config.ts
@@ -5,6 +5,7 @@ import "@nomiclabs/hardhat-waffle";
 import "@typechain/hardhat";
 import "hardhat-gas-reporter";
 import "solidity-coverage";
+import "@openzeppelin/hardhat-upgrades";
 
 dotenv.config();
 
--- smartcontract/scripts/deploy.ts
+++ smartcontract/scripts/deploy.ts
@@ -3,7 +3,10 @@
 //
 // When running the script with `npx hardhat run <script>` you'll find the Hardhat
 // Runtime Environment's members available in the global scope.
-import { ethers } from "hardhat";
+import { ethers, upgrades } from "hardhat";
+import * as dotenv from "dotenv";
+
+dotenv.config();
 
 async function main() {
   // Hardhat always runs the compile task when running scripts with its command
@@ -13,12 +16,23 @@ async function main() {
   // manually to make sure everything is compiled
   // await hre.run('compile');
 
-  // We get the smartcontract to deploy
+  // We get the smartcontract to deploy.
   const GiftNFTCard = await ethers.getContractFactory("GiftNFTCard");
-  const gnftCard = await GiftNFTCard.deploy();
+  const deployedAddress = process.env.DEPLOYED_ADDRESS;
+  if (!deployedAddress) {
+    // Deploying the smart contract for the first time.
+    const gnftCard = await upgrades.deployProxy(GiftNFTCard);
+    await gnftCard.deployed();
 
-  await gnftCard.deployed();
-  console.log("GiftNFTCard deployed to:", gnftCard.address);
+    console.log("GiftNFTCard deployed to:", gnftCard.address);
+    console.log(
+      "Kindly keep the address safe. This is the address to which the smart contract can be upgraded to."
+    );
+  } else {
+    // Upgrading the smart contract.
+    await upgrades.upgradeProxy(deployedAddress, GiftNFTCard);
+    console.log("GiftNFTCard upgraded to:", deployedAddress);
+  }
 }
 
 // We recommend this pattern to be able to use async/await everywhere
