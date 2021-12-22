938a7aec0f3fa1e2f4b5b54ed774d3d5a05e33de
Sharad Chand <pepsighan@sharadchand.com>
2021-12-22 18:14:58 +0545

    Create a smartcontract project

---
+++ contract/hardhat.config.ts
@@ -0,0 +1,28 @@
+import * as dotenv from "dotenv";
+import { HardhatUserConfig } from "hardhat/config";
+import "@nomiclabs/hardhat-etherscan";
+import "@nomiclabs/hardhat-waffle";
+import "@typechain/hardhat";
+import "hardhat-gas-reporter";
+import "solidity-coverage";
+
+dotenv.config();
+
+// You need to export an object to set up your config
+// Go to https://hardhat.org/config/ to learn more
+const config: HardhatUserConfig = {
+  solidity: "0.8.4",
+  networks: {
+    metis: {
+      url: process.env.METIS_STARDUST_URL || "",
+      accounts:
+        process.env.PRIVATE_KEY !== undefined ? [process.env.PRIVATE_KEY] : [],
+    },
+  },
+  gasReporter: {
+    enabled: process.env.REPORT_GAS !== undefined,
+    currency: "USD",
+  },
+};
+
+export default config;
