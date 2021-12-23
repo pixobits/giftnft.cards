cb2056b2691731a09c878d69b3b85e08ac69726f
Sharad Chand <pepsighan@sharadchand.com>
2021-12-23 17:08:53 +0545

    Add hook to fetch my gifts

--- web/store/gifts.ts
+++ web/store/gifts.ts
@@ -2,15 +2,31 @@ import { useEthers } from "store/metamask";
 import { useQuery } from "react-query";
 import { ethers } from "ethers";
 import config from "config";
+import { useAccount } from "store/account";
 
+/**
+ * Fetch all the gift cards owned by the current selected account.
+ */
 export function useMyGifts() {
   const eth = useEthers();
 
-  useQuery("use-my-gifts", async () => {
+  return useQuery("use-my-gifts", async () => {
     const contract = new ethers.Contract(
       config.CONTRACT_ADDRESS,
       config.CONTRACT_ABI,
       eth
+    );
+
+    const accountId = useAccount.getState().accountId;
+    if (!accountId) {
+      return;
+    }
+
+    const giftsCount = await contract.balanceOf(accountId);
+    return await Promise.all(
+      Array(giftsCount)
+        .fill(0)
+        .map(async (_, index) => contract.getGiftCardByIndex(index))
     );
   });
 }
