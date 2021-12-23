a4654cea1f8ce27153112d23dba908b63d68cf49
Sharad Chand <pepsighan@sharadchand.com>
2021-12-23 17:30:47 +0545

    Add hook to mint a new gift card

--- web/store/gifts.ts
+++ web/store/gifts.ts
@@ -1,8 +1,9 @@
 import { useEthers } from "store/metamask";
-import { useQuery } from "react-query";
+import { useQuery, useQueryClient } from "react-query";
 import { ethers } from "ethers";
 import config from "config";
 import { useAccount } from "store/account";
+import { useCallback } from "react";
 
 /**
  * Fetch all the gift cards owned by the current selected account.
@@ -51,4 +52,47 @@ export function useSentGifts() {
         .map(async (_, index) => contract.getSentGiftCardByIndex(index))
     );
   });
+}
+
+type NewGiftCard = {
+  recipient: string;
+  amount: string;
+  imageDataUrl: string;
+  message: string;
+  signedBy: string;
+};
+
+/**
+ * Mint a new gift card.
+ */
+export function useMintGiftCard() {
+  const eth = useEthers();
+  const client = useQueryClient();
+
+  return useCallback(
+    async (arg: NewGiftCard) => {
+      const signer = eth.getSigner();
+      const contract = new ethers.Contract(
+        config.CONTRACT_ADDRESS,
+        config.CONTRACT_ABI,
+        signer
+      );
+
+      await contract.safeMint(
+        arg.recipient,
+        arg.imageDataUrl,
+        arg.message,
+        arg.signedBy,
+        // Send the following amount to be wrapped in the gift card.
+        { value: arg.amount }
+      );
+
+      // Refetch the gifts.
+      await Promise.all([
+        client.invalidateQueries("use-my-gifts"),
+        client.invalidateQueries("use-sent-gifts"),
+      ]);
+    },
+    [client, eth]
+  );
 }
