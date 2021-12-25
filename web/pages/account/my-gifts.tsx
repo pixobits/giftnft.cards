c7d33387ce764da626de94d902222b79fe6ba387
Sharad Chand <pepsighan@sharadchand.com>
2021-12-25 13:38:54 +0545

    List the gifts owned by the user

--- web/pages/account/my-gifts.tsx
+++ web/pages/account/my-gifts.tsx
@@ -1,8 +1,11 @@
-import { Typography } from "@mui/material";
+import { Box, Container, Grid, Paper, Typography } from "@mui/material";
 import { NextSeo } from "next-seo";
 import Navigation from "components/Navigation";
+import { useMyGifts } from "store/gifts";
 
 export default function MyGifts() {
+  const { data: gifts } = useMyGifts();
+
   return (
     <>
       <NextSeo title="My Gifts" />
@@ -10,6 +13,23 @@ export default function MyGifts() {
       <Typography variant="h5" textAlign="center" sx={{ mt: 4 }}>
         My Gifts
       </Typography>
+
+      <Container maxWidth="lg" sx={{ mt: 8, mb: 4 }}>
+        <Grid container spacing={4}>
+          {(gifts ?? []).map((it) => (
+            <Grid item key={it.tokenId._hex}>
+              <Paper sx={{ width: 400, height: 550, mb: 2 }}>
+                <Box
+                  component="img"
+                  src={it.imageDataUrl}
+                  alt={it.signedBy}
+                  sx={{ width: 400, height: 550 }}
+                />
+              </Paper>
+            </Grid>
+          ))}
+        </Grid>
+      </Container>
     </>
   );
 }
