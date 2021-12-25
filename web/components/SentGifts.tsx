1675787e7be6557804ed03499874d7ffecd6e494
Sharad Chand <pepsighan@sharadchand.com>
2021-12-25 13:31:05 +0545

    List the gifts minted by the user

--- web/components/SentGifts.tsx
+++ web/components/SentGifts.tsx
@@ -1,13 +1,28 @@
-import { Typography } from "@mui/material";
+import { Box, Container, Grid, Paper, Typography } from "@mui/material";
 import { useSentGifts } from "store/gifts";
 
 export default function SentGifts() {
-  const data = useSentGifts();
+  const { data: gifts } = useSentGifts();
 
   return (
-    <>
-      <Typography>Sent Gifts</Typography>
-      {JSON.stringify(data)}
-    </>
+    <Container maxWidth="lg" sx={{ mt: 8, mb: 4 }}>
+      <Typography variant="h5" sx={{ mb: 2 }}>
+        Gifts you sent
+      </Typography>
+      <Grid container spacing={4}>
+        {(gifts ?? []).map((it) => (
+          <Grid item key={it.tokenId._hex}>
+            <Paper sx={{ width: 400 / 2, height: 550 / 2, mb: 2 }}>
+              <Box
+                component="img"
+                src={it.imageDataUrl}
+                alt={it.signedBy}
+                sx={{ width: 400 / 2, height: 550 / 2 }}
+              />
+            </Paper>
+          </Grid>
+        ))}
+      </Grid>
+    </Container>
   );
 }
