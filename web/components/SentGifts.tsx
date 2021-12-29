f9f98a7f778ab68bce65bb1c69070f5962e2c248
Sharad Chand <pepsighan@sharadchand.com>
2021-12-29 13:06:39 +0545

    Show empty message and loading spinner

--- web/components/SentGifts.tsx
+++ web/components/SentGifts.tsx
@@ -1,28 +1,49 @@
-import { Box, Container, Grid, Paper, Typography } from "@mui/material";
+import {
+  Alert,
+  Box,
+  CircularProgress,
+  Container,
+  Grid,
+  Paper,
+  Stack,
+  Typography,
+} from "@mui/material";
 import { useSentGifts } from "store/gifts";
 
 export default function SentGifts() {
-  const { data: gifts } = useSentGifts();
+  const { data: gifts, isLoading } = useSentGifts();
 
   return (
     <Container maxWidth="lg" sx={{ mt: 8, mb: 4 }}>
       <Typography variant="h5" sx={{ mb: 2 }}>
         Gifts you sent
       </Typography>
-      <Grid container spacing={4}>
-        {(gifts ?? []).map((it) => (
-          <Grid item key={it.tokenId._hex}>
-            <Paper sx={{ width: 300 / 1.5, height: 400 / 1.5, mb: 2 }}>
-              <Box
-                component="img"
-                src={it.imageDataUrl}
-                alt={it.signedBy}
-                sx={{ width: 300 / 1.5, height: 400 / 1.5 }}
-              />
-            </Paper>
-          </Grid>
-        ))}
-      </Grid>
+      {isLoading && (
+        <Stack alignItems="center">
+          <CircularProgress />
+        </Stack>
+      )}
+
+      {!isLoading && (gifts ?? []).length > 0 && (
+        <Grid container spacing={4}>
+          {(gifts ?? []).map((it) => (
+            <Grid item key={it.tokenId._hex}>
+              <Paper sx={{ width: 300 / 1.5, height: 400 / 1.5, mb: 2 }}>
+                <Box
+                  component="img"
+                  src={it.imageDataUrl}
+                  alt={it.signedBy}
+                  sx={{ width: 300 / 1.5, height: 400 / 1.5 }}
+                />
+              </Paper>
+            </Grid>
+          ))}
+        </Grid>
+      )}
+
+      {!isLoading && (gifts ?? []).length === 0 && (
+        <Alert severity="info">You have not sent any gifts yet.</Alert>
+      )}
     </Container>
   );
 }
