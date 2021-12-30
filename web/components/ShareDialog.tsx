047dd2ccc4119bd0a21fc3233491bca92f8fe6f1
Sharad Chand <pepsighan@sharadchand.com>
2021-12-30 16:54:33 +0545

    Add some functional share buttons

--- web/components/ShareDialog.tsx
+++ web/components/ShareDialog.tsx
@@ -1,5 +1,20 @@
-import { Box, Dialog, DialogContent, DialogTitle, Stack } from "@mui/material";
+import {
+  Box,
+  Dialog,
+  DialogContent,
+  DialogTitle,
+  Stack,
+  Typography,
+} from "@mui/material";
 import { GiftCard } from "store/gifts";
+import {
+  TelegramIcon,
+  TelegramShareButton,
+  TwitterIcon,
+  TwitterShareButton,
+  WhatsappIcon,
+  WhatsappShareButton,
+} from "react-share";
 
 type ShareDialogProps = {
   open: boolean;
@@ -15,6 +30,10 @@ export default function ShareDialog({
   onClose,
   giftCard,
 }: ShareDialogProps) {
+  const url = "https://giftnft.cards/";
+  const message =
+    "I have sent a Gift Card NFT to your account. To access and unwrap your gift, visit Gift NFT Cards.";
+
   return (
     <Dialog open={open} onClose={onClose}>
       <DialogTitle>Share</DialogTitle>
@@ -26,6 +45,29 @@ export default function ShareDialog({
             alt={giftCard.signedBy}
             sx={{ width: 300, height: 400 }}
           />
+        </Stack>
+
+        <Typography variant="subtitle2" textAlign="center" sx={{ mt: 2 }}>
+          Share to the person that you have sent a Gift Card NFT
+          <br /> to their account.
+        </Typography>
+        <Stack
+          direction="row"
+          justifyContent="center"
+          spacing={2}
+          sx={{ mt: 1 }}
+        >
+          <TwitterShareButton url={url} title={message}>
+            <TwitterIcon size={32} round />
+          </TwitterShareButton>
+
+          <WhatsappShareButton url={url} title={message}>
+            <WhatsappIcon size={32} round />
+          </WhatsappShareButton>
+
+          <TelegramShareButton url={url} title={message}>
+            <TelegramIcon size={32} round />
+          </TelegramShareButton>
         </Stack>
       </DialogContent>
     </Dialog>
